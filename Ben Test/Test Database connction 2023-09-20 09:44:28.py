fe876ec209f760c2cea349769e87eb0817380187
Ben202309141505 <Ben202309141505>
2023-09-21 02:03:47 +0000

    update

--- Ben Test/Test Database connction 2023-09-20 09:44:28.py
+++ Ben Test/Test Database connction 2023-09-20 09:44:28.py
@@ -24,3 +24,32 @@ remote_table = (spark.read
 
 display(remote_table)
 
+
+# COMMAND ----------
+
+pwdstring = dbutils.secrets.get(scope="ddpcn-key-vault-secrets", key="dwhchinaadmin-secret")
+
+driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+
+connect_string2 = dbutils.secrets.get(scope="ddpcn-key-vault-secrets", key="dwhchinaadmin-connect")
+
+connect_string2.format(password=pwdstring)
+
+connect_string = "jdbc:sqlserver://mssql-ddpcn-dev02-chinaeast2.database.chinacloudapi.cn:1433;database=sqldb-ddpcn-dev01-chinaeast2;user=dwhchinaadmin;password={password}".format(password=pwdstring)
+
+table = "[dw_sfe].[Dim_Product]"
+
+remote_table = (spark.read
+  .format("jdbc")
+  .option("driver", driver)
+  .option("url", connect_string)
+  .option("dbtable", table)
+  .load()
+)
+
+display(remote_table)
+
+
+# COMMAND ----------
+
+# MAGIC %fs ls
